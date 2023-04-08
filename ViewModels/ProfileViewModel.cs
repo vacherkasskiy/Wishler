beefe7bb8472025bf8b1d9a41ac7c163291503a1
vacherkasskiy <vitalik.cherkasskiy@gmail.com>
2023-04-08 20:01:17 +0300

    profile page design improved

---
+++ ViewModels/ProfileViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Wishler.ViewModels;
+
+public class ProfileViewModel
+{
+    public int BoardsCreatedAmount { get; set; }
+    public int GroupsParticipatedAmount { get; set; }
+    public int FriendsAddedAmount { get; set; }
+}
