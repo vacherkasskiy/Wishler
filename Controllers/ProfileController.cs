446d38016cf5b8a5908b21405d3febf073bd0185
vacherkasskiy <vitalik.cherkasskiy@gmail.com>
2023-04-08 03:46:24 +0300

    base for profile added

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Wishler.Controllers;
+
+public class ProfileController : Controller
+{
+    [HttpGet]
+    [Authorize]
+    [Route("/user/{userId}/profile")]
+    public IActionResult Index(int userId)
+    {
+        return View();
+    }
+}
