b05acfdcceab194d1d0e610db690852be8393a5c
vacherkasskiy <vitalik.cherkasskiy@gmail.com>
2023-05-04 21:45:13 +0300

    board deletion now provided with pop-ups

---
+++ wwwroot/js/delete_board.js
@@ -0,0 +1,11 @@
+﻿let deleteBoardButtons = document.querySelectorAll(".board .delete");
+
+for (let i = 0; i < deleteBoardButtons.length; ++i) {
+    deleteBoardButtons[i].addEventListener("click", (event) => {
+        popUpObjName.innerHTML = event.target.id;
+        let groupId = event.target.parentNode.parentElement.id;
+        popUpDeleteButton.href = "boards/delete/" + groupId;
+        popUpBlock.style.display = "flex";
+        shadow.classList.add("active");
+    }, true);
+}
